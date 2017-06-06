7b4b0df909639e50922320ad2a5a99e4557dae81
slee0108 <slee0108@mymail.lausd.net>
2017-06-05 22:00:09 -0400

    add mystyle.css

--- Gallery.js
+++ Gallery.js
@@ -3,21 +3,21 @@ var pics = ["http://www.cutenessoverflow.com/wp-content/uploads/2016/06/l.jpg",
             "http://cfcdn.newsmax.com/Newsmax/files/58/58f7a913-a4a6-47d1-9f38-cf3599f8b2eb.jpg",
             "https://www.ucdavis.edu/sites/default/files/news/general-news/2016/apr/ohi-logo-consortium.jpg",
              "https://1.cdn.edl.io/zx0vgxMF6oWXOTpDc6FH5djem6wHNqKQcNOuhtZTMYDAIaKD.png"];
-var divlocation = 0;
+var divLocation = 0;
 
 
 function goLeft(){
-  divlocation = divlocation - 1;
+  divLocation = divLocation - 1;
   
-  if(location > 0){
-    document.getElementById("pic").scr=pics[locations];
+  if(divLocation > 0){
+    document.getElementById("pic").src=pics[divLocation];
   }
 }
 
 function goRight(){
-  divlocation=divlocation + 2;
+  divLocation = divLocation + 1;
   
-  if(location < pics.lenght){
-    document.getElementById("pic").src=pics[location];
+  if(divLocation < pics.length){
+    document.getElementById("pic").src=pics[divLocation];
   }
 }
